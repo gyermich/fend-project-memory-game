8be064e825543441675f1321e2ccdd0e63b51286
gyermich <yermichevagalina@gmail.com>
2018-05-20 16:22:20 -0700

    shuffle cards

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
  * Create a list that holds all of your cards
  */
 
+// get a NodeList of cards and convert it to an array to be able to shuffle
+let cards = [...document.querySelectorAll("li.card")];
 
 /*
  * Display the cards on the page
@@ -9,9 +11,21 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+ shuffledCards = shuffle(cards);
+
+let deck = document.querySelector(".deck");
+
+// clear contents of deck
+deck.innerHTML = '';
+
+// aapend shuffled cards to deck
+for (const card of shuffledCards) {
+    deck.appendChild(card);
+};
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
+
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -19,6 +33,7 @@ function shuffle(array) {
         currentIndex -= 1;
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
+        console.log(array, array[currentIndex]);
         array[randomIndex] = temporaryValue;
     }
 

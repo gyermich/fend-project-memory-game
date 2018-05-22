13594109882cc8930e57a5f9f9e2049e1727c143
gyermich <yermichevagalina@gmail.com>
2018-05-21 20:26:18 -0700

    add animations, moves and restart functionality

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@
 // get a NodeList of cards and convert it to an array to be able to shuffle
 let cards = [...document.querySelectorAll("li.card")];
 let openCards = [];
+let moves = 0;
+let deck = document.querySelector(".deck");
 
 /*
  * Display the cards on the page
@@ -12,17 +14,17 @@ let openCards = [];
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
- shuffledCards = shuffle(cards);
 
-let deck = document.querySelector(".deck");
-
-// clear contents of deck
-deck.innerHTML = '';
-
-// aapend shuffled cards to deck
-for (const card of shuffledCards) {
-    deck.appendChild(card);
-};
+function shuffleDeck() {
+    // shuffle cards
+    shuffledCards = shuffle(cards);
+    // clear contents of deck
+    deck.innerHTML = '';
+    // apend shuffled cards to deck
+    for (const card of shuffledCards) {
+        deck.appendChild(card);
+    };
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -64,9 +66,16 @@ function lockOpenCards() {
         card.classList.add("match");
     };
 }
+
 function hideOpenCards() {
     for (let card of openCards) {
         card.classList.remove("open", "show", "no-match");
+    };
+}
+
+function resetCards() {
+    for (let card of cards) {
+        card.classList.remove("open", "show", "no-match", "match");
     };
 }
 
@@ -74,34 +83,68 @@ function clearOpenCards() {
     openCards = [];
 }
 
-function checkMatch() {
+function shakeCards() {
+    for (let card of openCards) {
+        card.classList.add("no-match");
+    };
+}
+
+function zoomCards() {
+    for (let card of openCards) {
+        card.classList.add("zoom");
+    };
+}
+
+function isMatch() {
     const [card1, card2] = openCards;
     if (card1.firstElementChild.classList[1] !== card2.firstElementChild.classList[1]) {
-        card1.classList.add("no-match");
-        card2.classList.add("no-match");
+        shakeCards();
         return false;
     } else {
+        zoomCards();
         return true;
     }
 }
 
+function updateMovesCount() {
+    document.querySelector(".moves").textContent = moves;
+}
+
+shuffleDeck();
+
 deck.addEventListener('click', function(event) {
     event.preventDefault();
-
+    // check if the clicked element is li
     if (event.target.nodeName === "LI") {
         let card = event.target;
 
         displayCardSymbol(card);
         addToOpenCards(card);
 
+        // check if there are 2 open cards
         if (openCards.length == 2) {
-            if (checkMatch()) {
-                window.setTimeout(lockOpenCards(), 1000);
+            if (isMatch()) {
+                // if it's a match lock them open
+                lockOpenCards();
             } else {
+                // otherwise hide the cards
                 window.setTimeout(hideOpenCards, 1000);
             }
+            moves++
+            updateMovesCount();
+            // when done checking if it's a match clear list of open cards
             window.setTimeout(clearOpenCards, 1000);
+        };
+        if (deck.querySelectorAll(".match").length === 16) {
+            alert("YES!");
         }
-
     }
 });
+
+document.querySelector(".restart").addEventListener("click", function(event){
+    // reset moves
+    moves = 0;
+    updateMovesCount();
+    resetCards();
+    shuffleDeck();
+})

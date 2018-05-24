9849ef827526b981bb4fb0e5f99cf833fa14bf0b
galina <galina@energysavvy.com>
2018-05-24 16:04:57 -0700

    lint with jshint.com

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
 /*
  * Create a list that holds all of your cards
  */
-
 let openCards = [];
 let moves = 0;
-let deck = document.querySelector(".deck");
+let deck = document.querySelector('.deck');
 // get a NodeList of cards and convert it to an array to be able to shuffle
-let cards = [...document.querySelectorAll("li.card")];
+let cards = [...document.querySelectorAll('li.card')];
 let elapsedTime = 0;
 let playing = false;
 
 /*
  * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - shuffle the list of cards using the provided 'shuffle' method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
 
 function shuffleDeck() {
     // shuffle cards
-    shuffledCards = shuffle(cards);
+    let shuffledCards = shuffle(cards);
     // clear contents of deck
     deck.innerHTML = '';
     // apend shuffled cards to deck
-    for (const card of shuffledCards) {
+    shuffledCards.forEach(function(card) {
         deck.appendChild(card);
-    };
+    });
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -43,19 +43,9 @@ function shuffle(array) {
     return array;
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 function displayCardSymbol(card) {
-    card.classList.add("open", "show");
+    card.classList.add('open', 'show');
 }
 
 function addToOpenCards(card) {
@@ -66,21 +56,21 @@ function addToOpenCards(card) {
 }
 
 function lockOpenCards() {
-    for (let card of openCards) {
-        card.classList.add("match");
-    };
+    openCards.forEach(function(card) {
+        card.classList.add('match');
+    });
 }
 
 function hideOpenCards() {
-    for (let card of openCards) {
-        card.classList.remove("open", "show", "no-match");
-    };
+    openCards.forEach(function(card) {
+        card.classList.remove('open', 'show', 'no-match');
+    });
 }
 
 function resetCards() {
-    for (let card of cards) {
-        card.classList.remove("open", "show", "no-match", "match", "zoom");
-    };
+    cards.forEach(function(card) {
+        card.classList.remove('open', 'show', 'no-match', 'match', 'zoom');
+    });
 }
 
 function clearOpenCards() {
@@ -88,15 +78,15 @@ function clearOpenCards() {
 }
 
 function shakeCards() {
-    for (let card of openCards) {
-        card.classList.add("no-match");
-    };
+    openCards.forEach(function(card) {
+        card.classList.add('no-match');
+    });
 }
 
 function zoomCards() {
-    for (let card of openCards) {
-        card.classList.add("zoom");
-    };
+    openCards.forEach(function(card) {
+        card.classList.add('zoom');
+    });
 }
 
 function isMatch() {
@@ -111,23 +101,23 @@ function isMatch() {
 }
 
 function updateMovesCount() {
-    document.querySelector(".moves").textContent = moves;
+    document.querySelector('.moves').textContent = moves;
 }
 
 function deductOneStar(star) {
-    star.firstElementChild.classList.remove("fa-star");
-    star.firstElementChild.classList.add("fa-star-o");
+    star.firstElementChild.classList.remove('fa-star');
+    star.firstElementChild.classList.add('fa-star-o');
 }
 
 function resetStars(stars) {
-    for (let star of stars) {
-        star.firstElementChild.classList.remove("fa-star-o");
-        star.firstElementChild.classList.add("fa-star");
-    };
+    stars.forEach(function(star) {
+        star.firstElementChild.classList.remove('fa-star-o');
+        star.firstElementChild.classList.add('fa-star');
+    });
 }
 
 function updateStarRating() {
-    let stars = document.querySelector(".stars").children;
+    let stars = [...document.querySelector('.stars').children];
 
     if (moves > 15 && moves <= 30) {
         // deduct one star from rating
@@ -142,16 +132,16 @@ function updateStarRating() {
 }
 
 function showModalWithStats(header) {
-    starStats = document.querySelector(".stars").innerHTML;
-    moveStats = document.querySelector(".moves").textContent;
-    timeStats = document.querySelector(".timer").textContent;
+    let starStats = document.querySelector('.stars').innerHTML;
+    let moveStats = document.querySelector('.moves').textContent;
+    let timeStats = document.querySelector('.timer').textContent;
 
     document.querySelector('.modal-stars').innerHTML = starStats;
     document.querySelector('.modal-moves').textContent = moveStats;
     document.querySelector('.modal-time').textContent = timeStats;
     document.querySelector('.modal-header').textContent = header;
 
-    document.querySelector('.modal').style.display = "block";
+    document.querySelector('.modal').style.display = 'block';
 }
 
 shuffleDeck();
@@ -159,7 +149,7 @@ shuffleDeck();
 deck.addEventListener('click', function(event) {
     event.preventDefault();
     // check if the clicked element is li
-    if (event.target.nodeName === "LI") {
+    if (event.target.nodeName === 'LI') {
         playing = true;
         let card = event.target;
 
@@ -175,67 +165,69 @@ deck.addEventListener('click', function(event) {
                 // otherwise hide the cards
                 window.setTimeout(hideOpenCards, 800);
             }
-            moves++
+            // increment moves count
+            moves += 1;
             updateMovesCount();
+            // map star rating to the moves count
             updateStarRating();
             // clear list of open cards
             window.setTimeout(clearOpenCards, 800);
-        };
+        }
         // game over at 50 moves
         if (moves === 50) {
             playing = false;
             showModalWithStats('Game Over');
-        };
+        }
         // all cards are matched - game won
-        if (deck.querySelectorAll(".match").length === 16) {
+        if (deck.querySelectorAll('.match').length === 16) {
             playing = false;
             showModalWithStats('You Won!');
-        };
+        }
     }
 });
 
-function resetGame(){
+function resetGame() {
     playing = false;
     moves = 0;
     updateMovesCount();
     updateStarRating();
     resetCards();
     shuffleDeck();
-    document.querySelector(".timer").textContent = "Time: 00:00";
-    document.querySelector('.modal').style.display = "none";
+    document.querySelector('.timer').textContent = 'Time: 00:00';
+    document.querySelector('.modal').style.display = 'none';
 }
 
 /*
  * Reset Button
  */
 
-document.querySelector(".restart").addEventListener("click", resetGame)
+document.querySelector('.restart').addEventListener('click', resetGame);
 
 /*
  * Modal Reset Button
  */
 
-document.querySelector(".modal-restart").addEventListener("click", resetGame)
+document.querySelector('.modal-restart').addEventListener('click', resetGame);
 
 /*
  * Timer
  */
 
 function padTimeDisplay(value) {
-    let padding = "00";
+    let padding = '00';
     // pad the minutes/seconds display with 0, e.g. if value is '1', return '01'
     return padding.substring(value.toString().length) + value;
 }
 
-let timer = setInterval(function() {
+setInterval(function() {
     if (playing === true) {
 
-        elapsedTime++;
+        elapsedTime += 1;
         let minutes = Math.floor(elapsedTime / 60);
         let seconds = elapsedTime - minutes * 60;
 
         let minutes_display = padTimeDisplay(minutes);
         let seconds_display = padTimeDisplay(seconds);
-        document.querySelector(".timer").textContent = `Time: ${minutes_display}:${seconds_display}`;
+        document.querySelector('.timer').textContent = `Time: ${minutes_display}:${seconds_display}`;
     }
 }, 1000);
